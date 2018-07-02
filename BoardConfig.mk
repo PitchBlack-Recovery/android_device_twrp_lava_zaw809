d6779f8ebc1c3bcbe79ff814e5efa0b43d433765
Manish4586 <Manish.n.manish45@gmail.com>
2018-07-02 14:18:13 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,7 +17,7 @@ TARGET_CPU_VARIANT := cortex-a7
 BOARD_KERNEL_PAGESIZE := 2048
 TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel
 BOARD_CUSTOM_MKBOOTIMG := mkbootimg.mk
-# BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/bootimg.mk
+BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/bootimg.mk
 # BOARD_CUSTOM_BOOTIMG := true
 # BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x04000000 --tags_offset 0x0e000000 --board 1441186574 --mtk 1
 
@@ -39,7 +39,7 @@ BOARD_HAS_MTK_HARDWARE := true
 MTK_HARDWARE := true
 
 # TWRP stuff
-TW_THEME := portrait_hdpi
+TW_THEME := portrait_xhdpi
 TW_SDEXT_NO_EXT4 := true
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/android0/f_mass_storage/lun/file"
@@ -66,3 +66,6 @@ TW_MAX_BRIGHTNESS := 255
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery/etc/twrp.fstab
 BOARD_HAS_LARGE_FILESYSTEM := true
 
+#Official
+PB_OFFICIAL := true
+PB_FORCE_DD_FLASH := true
